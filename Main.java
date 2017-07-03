94567d9a3e436bd27cffa7a2f7aa9bb3f17c4200
Noornihar <niharniluajn@gmail.com>
2017-07-03 15:28:40 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+# pos
+import java.io.*;
+import java.util.*;
+public class Main
+{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    Integer n=sc.nextInt();
+    if(n!=0)
+    {
+      
+
+    if(n>0)
+    {
+      
+      System.out.println("positive");
+    }
+    else
+    {
+      
+      System.out.println("negative");
+      }
+     }
+     else
+     {
+       
+       System.out.println("Zero");
+       }
+     }
+     }
+    
+
